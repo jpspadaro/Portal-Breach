f3de1b8688d1f6ef479bfa0f5793dcca483211ab
Jason Spadaro <jpspadaro@gmail.com>
2020-07-11 01:33:53 -0400

    Added most major systems except for combat. Should be music, combat, and features tomorrow.

---
+++ MonsterNode2D.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var speed = 20;
+
+signal escaped_monster;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	
+	#AI code
+	var side_move = 0;
+	if (self.position.x < 275):
+		side_move = speed/2;
+	if (self.position.x > 480):
+		side_move = speed * -1/2;
+	move_and_collide(Vector2(side_move*delta,speed*delta));
+	# print(self.get_instance_id(), self.position)
+	
+	if (self.position.y > 488 && (self.position.x > 200 && self.position.x < 480)):
+		emit_signal("escaped_monster");
+		self.queue_free();
--- main.gd
+++ main.gd
@@ -5,6 +5,12 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
+var monster_scene = preload("res://MonsterNode2D.tscn")
+
+var event_timer = 0;
+var wave = 1;
+var monster_rate = 10; #accellerates
+var damage = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,6 +20,26 @@ func check_quit():
 	if (Input.is_action_pressed("quit")):
 		get_tree().quit()
 
+func _on_MonsterNode2D_escaped_monster():
+	damage= damage + 1
+	get_node("ui/rightPanel/DamageRect").set_size(Vector2(15,damage+4))
+	
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	check_quit();
+	
+	event_timer = event_timer + delta;
+	
+	#get_node("ui/bottomPanel/info")
+	
+	if (event_timer > (wave*monster_rate)):
+		var monster = monster_scene.instance()
+		add_child(monster);
+		monster.translate(Vector2(200+rand_range(0,400),100));
+		monster.connect("escaped_monster",self,"_on_MonsterNode2D_escaped_monster")
+		wave += 1;
+		monster_rate = monster_rate - .2
+		if (monster_rate < .6): monster_rate = .6
+		print("Monster Rate:", monster_rate)
+	
